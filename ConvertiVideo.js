8fac4807d13c2d67fe73b1ace9049b3e06f06b68
Davide Vitiello <davidevitiello@iMac-di-Davide.local>
2020-10-10 21:21:45 +0200

    Aggiunto il frontend, cambiato il backend

---
+++ ConvertiVideo.js
@@ -0,0 +1,125 @@
+
+const ffmpeg = require('fluent-ffmpeg');
+const imagic = require('imagemagick');
+const { mkdir, rmdir, unlink } = require("fs").promises
+const uuid = require("uuid").v4
+let cartelle = [];
+
+
+
+
+async function convertVideo(nome, testo) {
+    let cartella = await generaImmagini(testo, nome);
+
+    return new Promise((resolve, reject) => {
+        ffmpeg()
+            .input(`tmp/${cartella}/img-%03d.png`)
+            .outputOption("-framerate 30")
+            .videoFilter("setpts=3.0*PTS")
+            .outputOption("-pix_fmt yuv420p")
+            .output(`tmp/${cartella}/scritta.mp4`).on("error", console.log).on("end", async () => {
+
+                ffmpeg()
+                    .input("Video/background.mp4")
+                    .input(`tmp/${cartella}/scritta.mp4`)
+                    .inputOption("-itsoffset 3")
+                    .complexFilter(
+                        [
+                            "[1:v]colorkey=0x00ff00:0.3:0.2[ckout]",
+                            "[0:v][ckout]overlay=(W-w)/2:(H-h)/2"
+                        ])
+                    .audioCodec("copy")
+                    .output(`tmp/${cartella}/${nome}.tmp.mp4`).on("end", () => {
+
+                        ffmpeg()
+                            .input(`tmp/${cartella}/${nome}.tmp.mp4`)
+                            .input(`Video/giallo.mp4`)
+                            .duration(5)
+                            .complexFilter(
+                                [
+                                    "[1:v]colorkey=0x00ff00:0.3:0.2[ckout]",
+                                    "[0:v][ckout]overlay=x='if(gte(t,0), -w+(t)*400, 3)':y=(H-h)/2",
+                                ])
+                            .output(`Final/${nome}.out.mp4`)
+                            .on("end", () => { resolve(`Final/${nome}.out.mp4`) })
+                            .on("error", () => { reject(err) })
+                            .run();
+
+
+
+
+
+                    }).run()
+
+
+
+
+
+
+
+
+
+
+            }).run()
+
+
+    });
+
+
+
+
+
+
+
+}
+
+
+async function generaImmagini(testo, nome) {
+
+    let nomeCartella = uuid()
+
+    cartelle.push({ cartella: nomeCartella, data: Date(), nome });
+
+    mkdir("tmp/" + nomeCartella)
+
+    let t = testo.split("");
+    let promises = []
+
+    let letterePrec = "";
+    for (let i = 0; i < t.length; i++) {
+        let lettera = t[i];
+        letterePrec += lettera;
+        let n = aggiungi0(i);
+        let p = convertiImmagine(["Assets/Images/green.png", "-gravity", "Center", "-pointsize", "30", "-font", "Assets/font.ttf", "-fill", "white", "-pointsize", "80", "-annotate", "0", `${letterePrec}`, `tmp/${nomeCartella}/img-${n}.png`])
+        promises.push(p)
+    }
+
+    await Promise.all(promises)
+
+
+    return nomeCartella;
+
+}
+
+
+
+function convertiImmagine(args) {
+    return new Promise((resolve, reject) => {
+        imagic.convert(args, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(data);
+        });
+    });
+}
+
+function aggiungi0(num) {
+    var str = "" + num
+    var pad = "000"
+    var ans = pad.substring(0, pad.length - str.length) + str
+    return ans
+}
+
+module.exports = { convertVideo }
--- index.js
+++ index.js
@@ -1,131 +1,28 @@
-const { spawn } = require('child_process');
-const ffmpeg = require('fluent-ffmpeg');
-const imagic = require('imagemagick');
-const { mkdir } = require("fs").promises
+const express = require("express");
+const { convertVideo } = require("./ConvertiVideo");
 const uuid = require("uuid").v4
-let cartelle = [];
-let nome = uuid();
+const { readFile } = require("fs").promises
+
+const app = express();
+
+app.use(express.static("public"))
+
+
+let port = process.env.PORT || 3000;
+
+app.get("/video", async (req, res) => {
+
+    let testo = req.query.testo;
+    let nome = uuid();
+    await convertVideo(nome, testo);
+    let file = await readFile(`Final/${nome}.out.mp4`)
+    res.setHeader('Content-Disposition', `attachment;filename=${nome}.mp4`);
+    res.setHeader("Content-Type", "video/mp4");
+    res.send(file);
+})
 
 
 
 
+app.listen(port, () => { console.log(`http://localhost:${port}`) })
 
-
-
-
-
-
-
-ffmpeg()
-    .input("Video/background.mp4")
-    .input("Video/giallo.mp4")
-
-    .complexFilter(
-        [
-            "[1:v]colorkey=0x00ff00:0.3:0.2[ckout]",
-            "[0:v][ckout]overlay=x='if(gte(t,0), -w+(t)*400, 3)':y=(H-h)/2",
-        ])
-    .output(`tmp/${nome}.mp4`).on("error", console.log).on("end", async (a) => {
-
-        let cartella = await generaImmagini("Hello World");
-        ffmpeg()
-            .input(`tmp/${cartella}/img-%03d.png`)
-            .outputOption("-framerate 30")
-            .duration("5")
-            .videoFilter("setpts=5.0*PTS")
-            .outputOption("-pix_fmt yuv420p")
-            .output(`tmp/${cartella}/scritta.mp4`).on("error", console.log).on("end", () => {
-
-
-                let command = ffmpeg().input(`tmp/${nome}.mp4`).input(`tmp/${cartella}/scritta.mp4`)
-
-                    .complexFilter(
-                        [
-                            "[1:v]colorkey=0x000000:0.3:0.2[ckout]",
-                            "[0:v][ckout]overlay=(W-w)/2:(H-h)/2"
-                        ])
-                    .audioCodec("copy")
-                    .output(`Final/${nome}.output.mp4`);
-
-                command.on('start', function (commandLine) {
-                    console.error('Spawned Ffmpeg with command: ' + commandLine);
-                });
-                command.duration("5s")
-
-                command.run();
-
-                //spawn(`open Final/${nome}.output.mp4`)
-
-            }).run()
-
-
-
-
-
-
-
-
-
-
-
-    }).run();
-
-async function generaImmagini(testo) {
-
-    let nomeCartella = uuid()
-
-    cartelle.push({ nome: nomeCartella, data: Date() });
-
-    mkdir("tmp/" + nomeCartella)
-
-    let t = testo.split("");
-    let promises = []
-
-    let letterePrec = "";
-    for (let i = 0; i < t.length; i++) {
-        let lettera = t[i];
-        letterePrec += lettera;
-        let n = aggiungi0(i);
-        let p = convertiImmagine(["Assets/Images/green.png", "-gravity", "Center", "-pointsize", "30", "-font", "Assets/font.ttf", "-fill", "white", "-pointsize", "80", "-annotate", "0", `${letterePrec}`, `tmp/${nomeCartella}/img-${n}.png`])
-        promises.push(p)
-    }
-
-    await Promise.all(promises)
-
-
-    return nomeCartella;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-function convertiImmagine(args) {
-    return new Promise((resolve, reject) => {
-        imagic.convert(args, (err, data) => {
-            if (err) {
-                return reject(err);
-            }
-
-            resolve(data);
-        });
-    });
-}
-
-function aggiungi0(num) {
-    var str = "" + num
-    var pad = "000"
-    var ans = pad.substring(0, pad.length - str.length) + str
-    return ans
-}
